a97e982b1121e62c4ece85c646402723caefbf87
Anabella <ana.da.rocha@hotmail.com>
2020-08-30 00:06:16 -0300

    Agregando funcionalidad en buscador y videos sugeridos

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {Grid} from '@material-ui/core';
+import {SearchBar, VideoList, VideoDetail} from './components';
+import youtube from './api/youtube';
+
+class App extends Component {
+    state = {
+        videos: [],
+        selectedVideo: null
+    }
+    componentDidMount(){
+        this.handleSubmit('algorithms')
+    }
+    onVideoSelect = video => {
+        this.setState({selectedVideo: video})
+    }
+    handleSubmit = async searchTerm => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyCj45KDbLPMYVn1bdtZHlMGRr7bZnKWkOc',
+                q: searchTerm
+            }
+        });
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+    }
+    render(){
+        const {selectedVideo, videos} = this.state;
+        return(
+            <Grid justify='center' container spacing={10}>
+            <Grid item xs={12}>
+            <Grid container spacing={10}>
+            <Grid item xs={12}>
+            <SearchBar onFormSubmit={this.handleSubmit}/>
+            </Grid>
+            <Grid item xs={8}>
+            <VideoDetail video={selectedVideo}/>
+            
+            </Grid>
+            <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+            </Grid>
+            </Grid>
+            </Grid>
+            </Grid>
+            )
+        }
+    }
+    
+    export default App;

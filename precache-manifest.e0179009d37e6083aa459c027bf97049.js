314a4001f29b72ed572f356c2f8080d3d626c2f0
Anabella <ana.da.rocha@hotmail.com>
2020-09-14 15:44:34 -0300

    Updates

---
+++ precache-manifest.e0179009d37e6083aa459c027bf97049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a729883b935cbdbc2e91052ea09f848",
+    "url": "/youtube-clon/index.html"
+  },
+  {
+    "revision": "4427c5d1f374e7c57780",
+    "url": "/youtube-clon/static/css/main.adabba99.chunk.css"
+  },
+  {
+    "revision": "a8b3d408e3cb471a2325",
+    "url": "/youtube-clon/static/js/2.48dfc627.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clon/static/js/2.48dfc627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4427c5d1f374e7c57780",
+    "url": "/youtube-clon/static/js/main.3c5e63f6.chunk.js"
+  },
+  {
+    "revision": "1cd729105b34099de88d",
+    "url": "/youtube-clon/static/js/runtime-main.414b54c0.js"
+  }
+]);
